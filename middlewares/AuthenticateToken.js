3a177eb4dbbdda3a63e2b97a0608bf58c9efc773
Rishia0112 <riba.bravo.up@phinmaed.com>
2024-11-08 22:36:27 +0800

    asfsdfad

--- middlewares/AuthenticateToken.js
+++ middlewares/AuthenticateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authenticateToken = (req, res, next) => {
   const token =
     req.headers.authorization && req.headers.authorization.split(" ")[1];
-  if (!token) return res.json({ error: "User not logged in!" });
+  if (!token) return res.json({ error: "User not logged in hehe!" });
 
   jwt.verify(token, process.env.SECRET_LOGIN_KEY, async (err, decoded) => {
     if (decoded) {
